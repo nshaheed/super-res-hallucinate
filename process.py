2b5a5e634306e917763b9ab1e1370cade774f438
nshaheed <nicholasshaheed@gmail.com>
2024-02-19 04:40:54 -0800

    updated to call the sr multiple times

--- process.py
+++ process.py
@@ -9,7 +9,7 @@ import audiosr
 
 # takes in an audio file and preps it for being super-res'd
 
-def process_audio(input_file, output_file, speed_factor=1.0, normalization=True):
+def process_audio(base_name, input_file, model, iter_num=0, speed_factor=1.0, normalization=True):
     # Load the audio file
     audio = AudioSegment.from_wav(input_file)
 
@@ -40,7 +40,6 @@ def process_audio(input_file, output_file, speed_factor=1.0, normalization=True)
             split.export(output_path, format="wav")
             split_count = i
 
-        # keep this?
         entire_audio_path = os.path.join(dir, 'file_full')
         audio.export(entire_audio_path)
 
@@ -48,11 +47,6 @@ def process_audio(input_file, output_file, speed_factor=1.0, normalization=True)
         # TODO support save patch in config (or use hydra dirs)
         save_path = 'test'
         os.makedirs(save_path, exist_ok=True)
-
-        # load model
-        # TODO can move this out?
-        # TODO device and model name are hydra configs
-        model = audiosr.build_model(model_name='basic',device='cpu')
 
         # empty array to append
         full_audio = np.empty([1,1,0])
@@ -81,8 +75,11 @@ def process_audio(input_file, output_file, speed_factor=1.0, normalization=True)
             full_audio = np.append(full_audio, waveform, axis=2)
 
         # save output to file
-        name = os.path.splitext(os.path.basename(entire_audio_path))[0] + '_AudioSR_Processed_48K'
+        # name = os.path.splitext(os.path.basename(entire_audio_path))[0] + '_AudioSR_Processed_48K'
+        name = f'{base_name}-{iter_num:03}.wav'
         audiosr.save_wave(full_audio, inputpath=entire_audio_path, savepath=save_path, name=name, samplerate=48000)
+
+        return os.path.join(save_path, name)
 
 
 def speed_change(sound, speed=1.0):
@@ -96,6 +93,25 @@ def speed_change(sound, speed=1.0):
     # so that regular playback programs will work right. They often only
     # know how to play audio at standard frame rate (like 44.1k)
     return sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
+
+
+def calc_speed(shift, steps):
+    return shift**(1.0/steps)
+
+# call process_audio several times to slowly shift it down to the desired freq
+def slow_shift(input_file, shift=0.5, steps=12):
+    speed = calc_speed(shift, steps)
+
+    # load model
+    # TODO can move this out?
+    # TODO device and model name are hydra configs
+    model = audiosr.build_model(model_name='basic',device='cpu')
+
+    for i in range(steps):
+        # process_audio(input_file, output_file, model, speed_factor=speed, normalization=True)
+
+        input_file = process_audio(base_name, input_file, model, iter_num=i, speed_factor=speed, normalization=True)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -111,4 +127,5 @@ if __name__ == "__main__":
         print("Error: speed_factor must be a valid float")
         sys.exit(1)
 
-    process_audio(input_file, output_file, speed_factor=speed_factor, normalization=True)
+    # process_audio(input_file, output_file, speed_factor=speed_factor, normalization=True)
+    slow_shift(input_file, shift=0.5, steps=1)
